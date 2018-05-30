223eea8dc3820c987161a00d35ceb78d760f3daa
Oliver Evans <oevans@lbl.gov>
2018-05-29 19:22:32 -0700

    Change .gitignore and setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from distutils.core import setup
 
 setup(
-    name='ipcluster_magic',
+    name='ipcluster_magics',
     version='0.1',
     license='BSD',
     description='IPyParallel SLURM Magic for Cori @ NERSC',
-    packages={'ipcluster_magic'},
+    packages={'ipcluster_magics'},
     install_requires=[
         'bqplot>=0.10.2',
         'ipython>=6.1.0',
         'ipywidgets>=7.2',
         'numpy>=1.12',
         'pandas>=0.20.3',
-        'qgrida>=1.0.2'
+        'qgrid>=1.0.2'
     ]
 )
